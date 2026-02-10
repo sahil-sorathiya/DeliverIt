625c32caa74673063831bfca599512a593ca9b07
Sahil Patel <sahil.patel4125@gmail.com>
2026-02-10 09:28:12 +0530

    Debugging-JDBC-Driver

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
+./apache-tomcat-10.1.49/bin/shudown.sh
+
 mvn clean package
 
 cp "./target/Deliverit-$1.war" "./apache-tomcat-10.1.49/webapps"
+
+git add .
+git commit -m "$2"
+git push -u origin main
+
+./apache-tomcat-10.1.49/bin/startup.sh
--- src/main/java/com/sahilpateldev/MyServlet.java
+++ src/main/java/com/sahilpateldev/MyServlet.java
@@ -6,9 +6,37 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.sql.*;
+import java.util.Properties;
+
 public class MyServlet extends HttpServlet {
+    @Override
     protected void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         System.out.println("Hello");
+
+        try {
+            String url = "jdbc:postgresql://127.0.0.1:5432/todo";
+            Properties properties = new Properties();
+            properties.setProperty("user", "postgresql");
+            properties.setProperty("password", "4100");
+
+            Connection connection = DriverManager.getConnection(url, properties);
+
+            PreparedStatement ps = connection.prepareStatement("CREATE DATABASE test");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                res.setContentType("text/html");
+                res.getWriter().print(rs.getString((1)));
+            }
+
+            rs.close();
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
         res.setContentType("text/html");
         res.getWriter().print("Hello from MyServlet");
     }
